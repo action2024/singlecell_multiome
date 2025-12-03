9855c3fd27c15dbba94981b264b5c4c712d0f68f
action2024 <160172934+action2024@users.noreply.github.com>
2025-12-03 15:03:29 -0500

    Create cellranger-arc.sh

---
+++ src/cellranger-arc.sh
@@ -0,0 +1,4 @@
+# install cellranger-arc
+wget -O cellranger-arc-2.1.0.tar.gz "https://cf.10xgenomics.com/releases/cell-arc/cellranger-arc-2.1.0.tar.gz?Expires=1764835084&Key-Pair-Id=APKAI7S6A5RYOXBWRPDA&Signature=bLNnGf1c~0J0TFLyB7SAVGrE1BMOf8iu4-r2hJ-33MsUdCffstIFmRHBsDE71ZF6lCr7kUpFt3VT8QRcrfBppddfGXI-CCaIvT2xYQADXi8qr1Yw9HEboMxQk~cQrIlHzNZxLeBx7pRJm3PRc38wE~lt30qh3OwbSAWzF8-fhSZSkcnl1h-TuTr1fOQi5Pz8ZuilrLjgygwspLqI4W~YmUxyS20gvkUW2Qu0n-Sog1TYrB1dv2-nqf8MCkVoa~3MeesBl15gNV9dRQrk-2bMgi9p1d5-V69Z8GGFFlKiTc167oc~eTjkQueG55fyXuF0i-BIzhhGxJJyEs8t1qBpGw__"
+# unzip 
+tar -xzvf cellranger-arc-2.1.0.tar.gz
