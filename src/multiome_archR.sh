4de634ad0a69e4d000e8b37e8dc36f5b620fff5b
action2024 <160172934+action2024@users.noreply.github.com>
2026-01-17 14:30:49 -0500

    Create multiome_archR.sh

---
+++ src/multiome_archR.sh
@@ -0,0 +1,21 @@
+~/mambaforge/bin/mamba env create --file /cluster/home/liuji/T1D/multiome/src/multiome.yml -n multiome
+~/mambaforge/bin/mamba env update --file /cluster/home/liuji/T1D/multiome/src/multiome.yml -n multiome
+
+source ~/mambaforge/etc/profile.d/conda.sh
+conda activate multiome
+
+# Clustering
+inputpath=/cluster/home/liuji/T1D/multiome/src/batch/$samplename/outs
+outputdir=/cluster/home/liuji/T1D/multiome/analysis/$samplename/clustering
+mkdir -p $outputdir
+Rscript /cluster/home/liuji/T1D/multiome/src/R/01_CombinedClustering.R $inputpath $outputdir
+
+# ATAC peak calling
+archRpath=/cluster/home/liuji/T1D/multiome/analysis/$samplename/clustering
+outputdir=/cluster/home/liuji/T1D/multiome/analysis/$samplename/peakcalling/Clusters_Combined
+mkdir -p $outputdir
+Rscript /cluster/home/liuji/T1D/multiome/src/R/03_PeakCalling.R $archRpath $outputdir Clusters_Combined
+Rscript /cluster/home/liuji/T1D/multiome/src/R/03_PeakCalling.R $archRpath $outputdir Clusters_ATAC
+Rscript /cluster/home/liuji/T1D/multiome/src/R/03_PeakCalling.R $archRpath $outputdir Clusters_RNA
+
+
